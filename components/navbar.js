f3f03de67f9f61a1005b2680a26074bcc303fc61
Kamesh255 <91534792+Kamesh255@users.noreply.github.com>
2021-10-22 12:46:39 +0530

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,10 @@
+function navbar(){
+    return ` 
+    <h3><a href="index.html">Home</a></h3>
+
+    <div id="options">
+        <h3><a href="ReceipeOfTheDay.html">Receipe Of The Day</a></h3>
+        <h3><a href="latestReceipe.html">Latest Receipe</a></h3> 
+     </div>`
+}
+export default navbar
